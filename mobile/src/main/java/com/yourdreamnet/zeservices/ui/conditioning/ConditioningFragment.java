55da881635775b3f73a0619b60b8d773342c4fea
Chris <chris@yourdreamnet.co.uk>
2018-12-15 10:40:30 +0000

    Add last scheduled time to the conditioning view.

--- mobile/src/main/java/com/yourdreamnet/zeservices/ui/conditioning/ConditioningFragment.java
+++ mobile/src/main/java/com/yourdreamnet/zeservices/ui/conditioning/ConditioningFragment.java
@@ -20,6 +20,8 @@ import com.yourdreamnet.zeservices.QueueSingleton;
 import com.yourdreamnet.zeservices.R;
 import com.yourdreamnet.zeservices.ZEServicesAPI;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class ConditioningFragment extends Fragment {
@@ -41,6 +43,36 @@ public class ConditioningFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        updateStatus();
+    }
+
+    private void updateStatus() {
+        ZEServicesAPI.AuthenticatedAPI api = getApi();
+        api.preconditionStatus(QueueSingleton.getQueue(), api.getCurrentVin()).
+            subscribe(result -> {
+                mViewModel.setConditioningData(result);
+                getActivity().runOnUiThread(() -> {
+                    TextView lastScheduled = getView().findViewById(R.id.last_scheduled);
+                    Date time = mViewModel.getLastPrecondition();
+                    if (time != null) {
+                        DateFormat dateFormat = android.text.format.DateFormat.getLongDateFormat(getContext());
+                        DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(getContext());
+                        String timeString = timeFormat.format(time) + " " + dateFormat.format(time);
+                        lastScheduled.setText(String.format(getString(R.string.last_scheduled), timeString));
+                    } else {
+                        lastScheduled.setText("");
+                    }
+                });
+            }, error -> {
+                Log.e("Conditioning", "Error getting last scheduled time", error);
+                TextView lastScheduled = getView().findViewById(R.id.last_scheduled);
+                lastScheduled.setText("");
+            });
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ConditioningViewModel.class);
@@ -52,6 +84,7 @@ public class ConditioningFragment extends Fragment {
                 subscribe(result -> Objects.requireNonNull(getActivity()).runOnUiThread(() -> {
                     TextView status = getView().findViewById(R.id.status);
                     status.setText(R.string.started_condition);
+                    updateStatus();
                 }), error -> Objects.requireNonNull(getActivity()).runOnUiThread(() -> {
                     Log.e("Conditioning", "Unable to pre-condition car", error);
                     TextView status = getView().findViewById(R.id.status);
--- mobile/src/main/java/com/yourdreamnet/zeservices/ui/conditioning/ConditioningViewModel.java
+++ mobile/src/main/java/com/yourdreamnet/zeservices/ui/conditioning/ConditioningViewModel.java
@@ -1,7 +1,42 @@
 package com.yourdreamnet.zeservices.ui.conditioning;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+
 import androidx.lifecycle.ViewModel;
 
 public class ConditioningViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private Date mLastPrecondition;
+    private String mLastType;  // Seen USER_REQUEST
+    private String mLastResult;  // Seen SUCCESS
+
+    boolean setConditioningData(JSONObject conditioningData) {
+        try {
+            mLastPrecondition = new Date(conditioningData.getLong("date"));
+            mLastType = conditioningData.getString("type");
+            mLastResult = conditioningData.getString("result");
+            return true;
+        } catch (JSONException e) {
+            Log.e("Conditioning", "Unable to parse conditioning data", e);
+            return false;
+        }
+    }
+
+    Date getLastPrecondition() {
+        return mLastPrecondition;
+    }
+
+    String getLastType() {
+        return mLastType;
+    }
+
+    String getLastResult() {
+        return mLastResult;
+    }
+
 }
